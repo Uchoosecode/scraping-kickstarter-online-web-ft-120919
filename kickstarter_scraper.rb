3f610048e8984aeba52f39c4e9373f9d6d71f941
Ayako Wiltshire <awittyl44@gmail.com>
2020-01-05 02:20:22 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -10,9 +10,13 @@ def create_project_hash
   
   projects = {}
   
-  kickstarter.css("li.project.grid_4").each {|projec 
-  
-  # write your code here
+  kickstarter.css("li.project.grid_4").each {|project| title = projects.css("h2.bbcard_name strong a").text
+    projects[title.to_sym] = { :image_link = projects.css("div.project-thumbnail a img").attribute("src").value,
+    :description = projects.css("p.bbcard_blurb").text,
+    :location = projects.css("ul.project-meta span.location-name").text,
+    :percent_funded = projects.css("ul.project-stats li.first.funded strong").text.gsub("%", "").to_i
+    }}
+    projects
 # binding.pry  
 end
 
