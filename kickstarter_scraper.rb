9cf4c5b180d31e458056f26e4706040b63460d54
Ayako Wiltshire <awittyl44@gmail.com>
2020-01-05 00:51:30 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,5 +1,20 @@
 # require libraries/modules here
+require 'nokogiri'
+require 'pry'
+
 
 def create_project_hash
+  html = File.read('fixtures/kickstarter.html')
+  
+  kickstarter = Nokogiri::HTML(html) 
+  
   # write your code here
+binding.pry  
 end
+
+
+
+#projects = kickstarter.css("li.project.grid_4")
+#titles = projects.css("h2.bbcard_name strong a").text
+# image_link = ("div.project-thumbnail a img").attribute("src").value 
+# description = project.css("p.bbcard_blurb").text
