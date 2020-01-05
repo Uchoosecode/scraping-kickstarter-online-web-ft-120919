7450cb52ae5b8b8d1c37f07c1b5ad5a5c90f872d
Ayako Wiltshire <awittyl44@gmail.com>
2020-01-05 01:12:57 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -17,4 +17,4 @@ end
 #projects = kickstarter.css("li.project.grid_4")
 #titles = projects.css("h2.bbcard_name strong a").text
 # image_link = projects.css("div.project-thumbnail a img").attribute("src").value 
-# description = project.css("p.bbcard_blurb").text
+# description = projects.css("p.bbcard_blurb").text
